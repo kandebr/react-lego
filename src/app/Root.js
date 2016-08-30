e33a3ee4f0ab04da9909e01d931b8c4e0c5c254b
Peter Mouland <peter.mouland@gmail.com>
2016-08-30 10:17:02 +0100

    add redux with promised middleware

--- src/app/Root.js
+++ src/app/Root.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router, browserHistory, createMemoryHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 import { makeRoutes } from './routes';
+import configureStore from './store/configureStore';
 import { isBrowser } from './utils';
 import debug from 'debug';
 
@@ -9,9 +12,15 @@ debug('lego:Root');
 
 // exported to be used in tests
 export const history = isBrowser ? browserHistory : createMemoryHistory();
+const store = configureStore(window.__INITIAL_STATE__); // eslint-disable-line
+const reduxHistory = syncHistoryWithStore(history, store);
 
 export default class Root extends React.Component {
   render() {
-    return <Router children={makeRoutes()} history={history} />;
+    return (
+      <Provider store={store}>
+        <Router history={reduxHistory} routes={makeRoutes()} />
+      </Provider>
+    );
   }
 }
--- src/server/middleware/set-router-context.js
+++ src/server/middleware/set-router-context.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { match, RouterContext } from 'react-router';
-import * as routes from '../../app/routes';
+import { Provider } from 'react-redux';
+import debug from 'debug';
+
+import configureStore from '../../app/store/configureStore';
+import { makeRoutes } from '../../app/routes';
 
 const setRouterContext = (req, res, next) => {
+  const log = debug('lego:set-router-context');
+
+  const store = configureStore();
+  const routes = makeRoutes(store);
+
   match({
-    routes: routes.makeRoutes(),
+    routes,
     location: req.url
   }, (error, redirect, renderProps) => {
+    log('match', req.url);
     if (error) {
       throw error;
     } else if (redirect) {
@@ -14,8 +24,14 @@ const setRouterContext = (req, res, next) => {
     } else {
       // path * will return a 404
       const isNotFound = renderProps.routes.find((route) => route.path === '*');
+      const InitialComponent = (
+        <Provider store={store}>
+          <RouterContext {...renderProps} />
+        </Provider>
+      );
+      res.initialState = store.getState(); // eslint-disable-line
       res.status(isNotFound ? 404 : 200); // eslint-disable-line
-      res.routerContext = <RouterContext {...renderProps} />; // eslint-disable-line
+      res.routerContext = res.renderPageToString(InitialComponent); // eslint-disable-line
       next();
     }
   });
