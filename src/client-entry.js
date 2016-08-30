355092a2de99802212978533b6d33936043bf599
Peter Mouland <peter.mouland@gmail.com>
2016-08-30 11:46:36 +0100

    add react-hot-loader v3

--- src/client-entry.js
+++ src/client-entry.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import debug from 'debug';
+import { AppContainer } from 'react-hot-loader';
 
 import Root from './app/Root';
 import './styles/app.scss';
@@ -10,8 +11,26 @@ debug.enable(process.env.DEBUG);
 const log = debug('lego:client-entry');
 log('Client environment', process.env);
 
+const rootEl = document.getElementById('html');
+export const App = (
+  <AppContainer>
+    <Root />
+  </AppContainer>
+);
+
 try {
-  ReactDOM.render(<Root />, document.getElementById('html'));
+  ReactDOM.render(App, rootEl);
+  if (module.hot) {
+    module.hot.accept('./app/Root', () => {
+      const NextApp = require('./app/Root').default; // eslint-disable-line
+      ReactDOM.render(
+        <AppContainer>
+          <NextApp />
+        </AppContainer>,
+        rootEl
+      );
+    });
+  }
 } catch (err) {
   log('Render error', err);
 }
