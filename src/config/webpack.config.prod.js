3b5020b0b8d93597a0fbe96eaa5625ea399c0339
Peter Mouland <peter.mouland@gmail.com>
2016-09-08 10:02:57 +0100

    update to webpack2

--- src/config/webpack.config.prod.js
+++ src/config/webpack.config.prod.js
@@ -1,11 +1,5 @@
-const webpack = require('webpack');
-
 const { SRC } = require('./paths');
 const defaultConfig = require('./webpack.common');
-
-defaultConfig.plugins.concat([
-  new webpack.optimize.UglifyJsPlugin({ minimize: true })
-]);
 
 const prodConfig = Object.assign({}, defaultConfig, {
   entry: { app: [`${SRC}/client-entry.js`] }
